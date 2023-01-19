b5b7a69c99a4ca3811c3ef31d63c249966095bd7
fareis2000 <53885395+fareis2000@users.noreply.github.com>
2023-01-19 00:07:22 -0300

    Media

---
+++ ex7.py
@@ -0,0 +1,4 @@
+nota1 = float(input('nota1: '))
+nota2 = float(input('nota2: '))
+media = (nota1 + nota2) / 2
+print('a 1 nota é: {:.1f}\na 2 nota é: {:.1f}\n a media é: {:.1f}'.format(nota1, nota2, media))
