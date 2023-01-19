2eb2838986c973f6f15a77afb0ba8887ed77d746
fareis2000 <53885395+fareis2000@users.noreply.github.com>
2023-01-19 00:09:03 -0300

    Tabuada

---
+++ ex9.py
@@ -0,0 +1,14 @@
+num = int(input('numero: '))
+num1  = num * 1
+num2  = num * 2
+num3  = num * 3
+num4  = num * 4
+num5  = num * 5
+num6  = num * 6
+num7  = num * 7
+num8  = num * 8
+num9  = num * 9
+num10 = num * 10
+print('-' * 12)
+print('{:2}x1 = {}\n{:2}x2 = {:2}\n{:2}x3 = {}\n{:2}x4 = {}\n{:2}x5 = {}\n{:2}x6 = {}\n{:2}x7 = {}\n{:2}x8 = {}\n{:2}x9 = {}\n{}x10 = {}'.format(num, num1, num, num2, num, num3, num, num4, num, num5, num, num6, num, num7, num, num8, num, num9, num, num10))
+print('-' * 12)
