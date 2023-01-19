3a737b3d7d5db3fe543d774d4cd6a621d2f1baf0
fareis2000 <53885395+fareis2000@users.noreply.github.com>
2023-01-19 00:13:05 -0300

    Add files via upload

---
+++ ex11.py
@@ -0,0 +1,6 @@
+larg = float(input('largura em m: '))
+alt = float(input('altura em m: '))
+area = alt * larg
+metro2 = 1 * 2
+tinta = area / metro2
+print('a area é: {}\na qtde de tinta necessaria é: {}\n'.format(area, tinta))
